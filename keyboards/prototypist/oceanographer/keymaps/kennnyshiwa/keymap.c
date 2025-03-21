6bde6ca05784a4a00e7f6f1e63ceae84680a1a9b
kennnyshiwa <kstjohn@kstj.us>
2025-03-20 21:54:13 -0400

    oops

--- keyboards/prototypist/oceanographer/keymaps/kennnyshiwa/keymap.c
+++ keyboards/prototypist/oceanographer/keymaps/kennnyshiwa/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_225_1(
     KC_F1,     KC_F2,    KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,
     KC_CAPS,   KC_1,     KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_ENT,
-    KC_SCRL,   KC_MINUS,     KC_EQUAL,      KC_BSLASH,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMMA,  KC_DOT,    CK_TOGG,   UG_SATU,   UG_TOGG,
+    KC_SCRL,   KC_MINUS,     KC_EQUAL,      KC_SLASH,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMMA,  KC_DOT,    CK_TOGG,   UG_SATU,   UG_TOGG,
     KC_NUM,    KC_LALT,  MO(_FN2),  _______,              KC_SPC,    KC_SPC,    KC_RALT,   KC_RCTL,             UG_HUED,   UG_SATD,   UG_HUEU
   ),
   [2] = LAYOUT_225_1(
